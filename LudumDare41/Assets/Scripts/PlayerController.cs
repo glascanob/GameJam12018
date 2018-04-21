807bbaf07122258fbb223a130d591de8d98d8635
Gabriel <glascanob@gmail.com>
2018-04-21 14:41:27 -0500

    Added initial third person controller and planet gravity

---
+++ LudumDare41/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+    public int moveSpeed = 10;
+    public float jumpForce = 0.1f;
+
+    [SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
+    [SerializeField] float m_AnimSpeedMultiplier = 1f;
+    [SerializeField] float m_GroundCheckDistance = 0.1f;
+    [SerializeField] float m_MoveSpeedMultiplier = 1f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    float m_ForwardAmount;
+    float m_TurnAmount;
+    bool m_IsGrounded;
+    Vector3 m_GroundNormal;
+
+    const float k_Half = 0.5f;
+
+    float y = 0;
+	// Use this for initialization
+	void Start () {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            y = jumpForce;
+            m_IsGrounded = false;
+        }
+	}
+    private void FixedUpdate()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float z = CrossPlatformInputManager.GetAxis("Vertical");
+
+
+        Vector3 moveDirection = new Vector3(x, y, z);
+        m_ForwardAmount = z * moveSpeed * Time.deltaTime;
+        m_TurnAmount = Mathf.Atan2(x * moveSpeed * Time.deltaTime, z * moveSpeed * Time.deltaTime);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + transform.TransformDirection(moveDirection)* moveSpeed * Time.deltaTime);
+        CheckGroundStatus();
+        UpdateAnimator(moveDirection);
+        y = 0;
+    }
+    void UpdateAnimator(Vector3 move)
+    {
+        // update the animator parameters
+        m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
+        m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
+        //m_Animator.SetBool("Crouch", m_Crouching);
+        m_Animator.SetBool("OnGround", m_IsGrounded);
+        if (!m_IsGrounded)
+        {
+            m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
+        }
+
+        // calculate which leg is behind, so as to leave that leg trailing in the jump animation
+        // (This code is reliant on the specific run cycle offset in our animations,
+        // and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
+        float runCycle =
+            Mathf.Repeat(
+                m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+        float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
+        if (m_IsGrounded)
+        {
+            m_Animator.SetFloat("JumpLeg", jumpLeg);
+        }
+
+        // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+        // which affects the movement speed because of the root motion.
+        if (m_IsGrounded && move.magnitude > 0)
+        {
+            m_Animator.speed = m_AnimSpeedMultiplier;
+        }
+        else
+        {
+            // don't use that while airborne
+            m_Animator.speed = 1;
+        }
+    }
+    public void OnAnimatorMove()
+    {
+        // we implement this function to override the default root motion.
+        // this allows us to modify the positional speed before it's applied.
+        if (m_IsGrounded && Time.deltaTime > 0)
+        {
+            Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
+
+            // we preserve the existing y part of the current velocity.
+            v.y = m_Rigidbody.velocity.y;
+            m_Rigidbody.velocity = v;
+        }
+    }
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+        {
+            m_GroundNormal = hitInfo.normal;
+            m_IsGrounded = true;
+            m_Animator.applyRootMotion = true;
+        }
+        else
+        {
+            m_IsGrounded = false;
+            m_GroundNormal = Vector3.up;
+            m_Animator.applyRootMotion = false;
+        }
+    }
+
+}
---
+++ LudumDare41/Assets/Scripts/WorldController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+    public GameObject world;
+    Rigidbody[] gravityObjects;
+    public int pullForce = 1;
+	// Use this for initialization
+	void Start () {
+        gravityObjects = world.GetComponentsInChildren<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+	private void FixedUpdate()
+	{
+        foreach (Rigidbody objectOnWorld in gravityObjects)
+        {
+            Vector3 forceDirection =  objectOnWorld.transform.position - transform.position;
+            Vector3 objectUp = objectOnWorld.transform.up;
+            objectOnWorld.AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+            Quaternion objectRotation = Quaternion.FromToRotation(objectUp, forceDirection) * objectOnWorld.transform.rotation;
+            objectOnWorld.transform.rotation = Quaternion.Slerp(objectOnWorld.transform.rotation, objectRotation, 50f * Time.deltaTime);
+        }
+	}
+}
