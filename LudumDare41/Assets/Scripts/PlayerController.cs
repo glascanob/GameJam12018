606d7f365a8959ec0ef99fba61eee8e8c1b3e5ad
Gabriel <glascanob@gmail.com>
2018-04-22 13:02:04 -0500

    Added sword slash

--- LudumDare41/Assets/Scripts/PlayerController.cs
+++ LudumDare41/Assets/Scripts/PlayerController.cs
@@ -12,19 +12,21 @@ public class PlayerController : MonoBehaviour {
     public GameObject castPoint;
     public GameObject planetContainer;
     Animator anim;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(CrossPlatformInputManager.GetButtonDown("Fire1"))
+        if(CrossPlatformInputManager.GetButtonDown("Fire2"))
         {
             CastSpell();
         }
-        if (CrossPlatformInputManager.GetButtonDown("Fire2"))
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
         {
             SlashSword();
         }
@@ -39,18 +41,18 @@ public class PlayerController : MonoBehaviour {
         float x = CrossPlatformInputManager.GetAxis("Horizontal");
         float z = CrossPlatformInputManager.GetAxis("Vertical");
         anim.SetFloat("Velocity", z);
-        Vector3 inverseVector = transform.InverseTransformVector(GetComponent<Rigidbody>().velocity);
+        Vector3 inverseVector = transform.InverseTransformVector(rb.velocity);
         if(Mathf.Approximately(z, 0))
         {
-            GetComponent<Rigidbody>().velocity *= 0.5f;
-            GetComponent<Rigidbody>().velocity = transform.TransformVector(new Vector3(0, inverseVector.y, 0));
+            rb.velocity *= 0.5f;
+            rb.velocity = transform.TransformVector(new Vector3(0, inverseVector.y, 0));
         }
         Vector3 moveDirection = new Vector3(x, y, z);
         transform.Rotate(0, x * rotationSpeed * Time.deltaTime, 0);
         Vector3 moveForce = transform.forward * z * moveSpeed;
         Vector3 jumpVector = transform.TransformVector(new Vector3(0, y, 0));
         moveForce += jumpVector;
-        GetComponent<Rigidbody>().AddForce(moveForce * Time.deltaTime, ForceMode.VelocityChange);
+        rb.AddForce(moveForce * Time.deltaTime, ForceMode.VelocityChange);
 
         //GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection)* moveSpeed * Time.deltaTime);
         y = 0;
@@ -59,6 +61,7 @@ public class PlayerController : MonoBehaviour {
     {
         anim.SetTrigger("Cast");
         GameObject orbe = Instantiate(spellPrefab, castPoint.transform.position, castPoint.transform.rotation, planetContainer.transform);
+        orbe.GetComponent<Rigidbody>().velocity = rb.velocity;
         orbe.GetComponent<Rigidbody>().AddForce(orbe.transform.forward * 200f);
     }
     public void SlashSword()
