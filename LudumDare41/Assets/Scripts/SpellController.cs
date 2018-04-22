d323ce9c8ca42816f25ef57c68e9b57972f14e28
Gabriel <glascanob@gmail.com>
2018-04-22 14:51:40 -0500

    Added fireSpell

--- LudumDare41/Assets/Scripts/SpellController.cs
+++ LudumDare41/Assets/Scripts/SpellController.cs
@@ -3,26 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpellController : MonoBehaviour {
-	// Use this for initialization
+    public Rigidbody rgbd;
+    public GameObject fieryParticle;
+    public GameObject smokeParticle;
+    public GameObject explosionParticle;
+    bool notHit = true;
+    // Use this for initialization
 	void Start () {
         StartCoroutine("LifeTimeCD");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Rigidbody>().AddForce(transform.forward * 5f * Time.deltaTime);
+        if (notHit)
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * 5f * Time.deltaTime);
+        }
 	}
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.tag == "Enemy")
         {
             other.GetComponent<EnemyController>().DeadState();
-            Destroy(gameObject);
+            ExplodeOnEnemy();
         }
 	}
+    IEnumerator ExplodeOnEnemy()
+    {
+        Explode();
+        yield return new WaitForSeconds(0.2f);
+        yield return new WaitUntil(() => !explosionParticle.GetComponent<ParticleSystem>().isPlaying);
+        Destroy(gameObject);
+    }
     IEnumerator LifeTimeCD()
     {
         yield return new WaitForSeconds(3f);
+        Explode();
+        yield return new WaitForSeconds(0.2f);
+        yield return new WaitUntil(() => !explosionParticle.GetComponent<ParticleSystem>().isPlaying);
         Destroy(gameObject);
+    }
+    void Explode()
+    {
+        notHit = false;
+        rgbd.constraints = RigidbodyConstraints.FreezeAll;
+        fieryParticle.SetActive(false);
+        smokeParticle.SetActive(false);
+        explosionParticle.SetActive(true);
     }
 }
